44dd56eecf200447848b3b5930f3d39858b3b83c
Grant Lorusso <grantlorusso@gmail.com>
2017-11-08 20:30:14 -0500

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-
-public class test {
-	public static void main(String args[])
-	{
-		Account test = new Account();
-		test.login("admin", "admin2");
-		test.createAccount("billy", "password123");
-	}
-}
